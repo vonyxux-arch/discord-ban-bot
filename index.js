9cc043a1d6a1cca82585ae84c4a6fc1a28d9d91b
vonyxux-arch <vonyxux@gmail.com>
2026-06-18 02:19:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
